8231d869d1efcac6fdf9174a625b9b0a3ee1f978
readleyj <readleyj@gmail.com>
2019-07-04 22:45:52 +0400

    Implemented TabularQAgent logic. Not tested

--- Game/Agents/TabularQAgent.py
+++ Game/Agents/TabularQAgent.py
@@ -1,5 +1,5 @@
 import random
-import Agent.Agent
+import Agent
 from Util import pos_to_coord
 import numpy as np
 
@@ -12,7 +12,7 @@ class TabularQAgent(Agent.Agent):
         self.gamma = gamma
         self.value_table = dict()
         self.current_state = None
-        self.current_action_index = None
+        self.action_index = None
 
     def make_move(self, available_moves, current_state):
         if (current_state not in self.value_table):
@@ -32,11 +32,18 @@ class TabularQAgent(Agent.Agent):
         action_coord = pos_to_coord(action_index)
         board.set_position_value(action_coord, self.side)
         self.current_state = board.current_state
-        self.current_action_index = action_index
+        self.action_index = action_index
 
     def learn_from_move(self, next_state, reward):
         action_coord = pos_to_coord(self.action_index)
-        action_value = self.value_table[self.current_state][action_index]
+        action_value = self.value_table[self.current_state][self.action_index]
+        max_action_index = np.argmax(self.value_table[next_state])
+
+        action_value += self.alpha * \
+            (reward + self.value_table[next_state]
+             [max_action_index] - action_value)
+
+        self.value_table[self.current_state][self.action_index] = action_value
 
     def stop_exploring(self):
         self.epsilon = 0
