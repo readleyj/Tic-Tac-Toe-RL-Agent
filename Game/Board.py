aa2439240bedda85983a80279a21874bd9d4ea74
readleyj <readleyj@gmail.com>
2019-07-06 15:48:15 +0400

    Update rewards when game is over

--- Game/Board.py
+++ Game/Board.py
@@ -8,7 +8,6 @@ class Board:
         self.all_moves = [[x, y] for x in range(3) for y in range(3)]
         self.valid_moves = self.all_moves
         self.winner = None
-        self.draw = False
 
     def is_position_empty(self, position):
         return position in self.available_moves
@@ -18,6 +17,7 @@ class Board:
         self.state = util.change_board_state(self.state, position, side)
         row, col = position
         self.board[row - 1][col - 1] = side
+        self.check_for_win(side)
         return self.state
 
     def change_board_state(current_state, position, side):
@@ -26,5 +26,9 @@ class Board:
             side + current_state[state_number + 1:]
         self.state = new_state
 
-    def check_for_win(self, letter):
-        pass
+    def check_for_win(self, side):
+        if (check_for_identical_rows):
+            self.winner = side
+        else:
+            if (not self.valid_moves):
+                self.winner = 'DRAW'
--- Game/Environment.py
+++ Game/Environment.py
@@ -11,24 +11,30 @@ class Environment:
         self.player1.learn_from_move(self.current_state, agent_one_reward)
         self.player2.learn_from_move(self.current_state, agent_two_reward)
 
+    def check_game_result(self):
+        if (self.board.result == self.player1.side):
+            self.set_rewards(1.0, -1.0)
+        else if (self.board.result == self.player2.side):
+            self.set_rewards(-1.0, 1.0)
+        else if (self.board.result == 'DRAW'):
+            self.set_rewards(-1.0, -1.0)
+        else:
+            self.set_rewards(0, 0)
+
     def run_game(self):
         while(self.board.winner is None and self.board.draw is False):
             self.player1.make_move(self.board.available_moves,
                                    self.board.current_state)
 
-            if (self.board.winner == self.player1.side):
-                self.set_rewards(1.0, -1.0)
+            self.check_game_result()
+            if (self.board.winner):
                 break
 
             self.player2.make_move(self.board.available_moves,
                                    self.board.current_state)
 
-            if (self.board.winner == self.player2.side):
-                self.set_rewards(-1.0, 1.0)
-                break
-
-            if (self.board.draw):
-                self.set_rewards(-1.0, -1.0)
+            self.check_game_result()
+            if(self.board.winner):
                 break
 
         def train(num_episodes=3000):
